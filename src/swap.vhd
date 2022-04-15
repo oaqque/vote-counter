eac030e5fc1741063af04f43d4fb5d6329028178
William Ye <oaqque@me.com>
2022-04-15 14:45:47 +1000

    + Finished swapping logic

--- src/swap.vhd
+++ src/swap.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.numeric_std.all;
 
 entity swap is
         port (swap_key      : in std_logic_vector(12 downto 0);
@@ -20,8 +21,10 @@ begin
         variable size : integer range 0 to 7 := conv_integer(swap_key(12 downto 10));
         variable p2 : integer range 0 to 7 := conv_integer(swap_key(9 downto 7));
         variable p1 : integer range 0 to 7 := conv_integer(swap_key(6 downto 4));
-        variable swp2 : std_logic_vector(size downto 0);
-        variable swp1 : std_logic_vector(size downto 0);
+        
+        variable swp2_buf : std_logic_vector(7 downto 0);
+        variable swp1_buf : std_logic_vector(7 downto 0);
+        variable tmp : std_logic;
     BEGIN
         case swap_key(3 downto 0) is
             when "0000" =>
@@ -30,8 +33,185 @@ begin
                 A1 <= D1;
                 A0 <= D0;
             when "0001" =>
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D0), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D1), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
                 A3 <= D3;
                 A2 <= D2;
-                A1 <= D1(7 downto p2 + size) & D0 (size downto p1) & D1(p2 downto 0);
+                A1 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A0 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+            when "0010" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D0), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D2), p1));
                 
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= D3;
+                A2 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A1 <= D1;
+                A0 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+            when "0011" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D0), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D3), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A2 <= D2;
+                A1 <= D1;
+                A0 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+            when "0100" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D1), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D0), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= D3;
+                A2 <= D2;
+                A1 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A0 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+            when "0101" => 
+                A3 <= D3;
+                A2 <= D2;
+                A1 <= D1;
+                A0 <= D0;
+            when "0111" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D1), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D3), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A2 <= D2;
+                A1 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A0 <= D0;
+            when "1000" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D2), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D0), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= D3;
+                A2 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A1 <= D1;
+                A0 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+            when "1001" =>
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D2), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D1), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= D3;
+                A2 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A1 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A0 <= D0;
+            when "1010" => 
+                A3 <= D3;
+                A2 <= D2;
+                A1 <= D1;
+                A0 <= D0;
+            when "1011" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D2), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D3), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A2 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A1 <= D1;
+                A0 <= D0;
+            when "1100" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D3), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D0), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A2 <= D2;
+                A1 <= D1;
+                A0 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+            when "1101" => 
+                swp2_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D3), p2));
+                swp1_buf := std_logic_vector(ROTATE_RIGHT(unsigned(D1), p1));
+                
+                for i in 0 to 7 loop
+                    if (i < size) then
+                        tmp := swp2_buf(i);
+                        swp2_buf(i) := swp1_buf(i);
+                        swp1_buf(i) := tmp;
+                    end if;
+                end loop;
+                
+                A3 <= std_logic_vector(ROTATE_LEFT(unsigned(swp2_buf), p2));
+                A2 <= D2;
+                A1 <= std_logic_vector(ROTATE_LEFT(unsigned(swp1_buf), p1));
+                A0 <= D0;
+            when "1111" => 
+                A3 <= D3;
+                A2 <= D2;
+                A1 <= D1;
+                A0 <= D0;
+            when others =>
+                A3 <= D3;
+                A2 <= D2;
+                A1 <= D1;
+                A0 <= D0;
+        end case;
+    END PROCESS;
 end behavioural;
